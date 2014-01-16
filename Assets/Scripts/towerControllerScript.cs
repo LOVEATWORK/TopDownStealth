e643e46defc89a57821eba41e80989d4e6ad550e
Lars Huring <lars.huring@loveatwork.se>
2014-01-16 23:36:17 +0100

    Added rotating tower and camera follow

---
+++ Assets/Scripts/towerControllerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class towerControllerScript : MonoBehaviour {
+
+	public float rotationSpeed = 2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		Spin ();
+	}
+
+	void Spin() {
+		transform.Rotate (0, 0, 60 * (rotationSpeed * Time.deltaTime));
+	}
+
+}

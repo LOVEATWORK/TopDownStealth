8c7d86749ce2ce4805d7b63554af269d3743ad5b
Lars Huring <lars.huring@loveatwork.se>
2014-01-16 23:19:56 +0100

    Got the movement for the character working - now for the rotating tower

--- Assets/Scripts/characterControllerScript.cs
+++ Assets/Scripts/characterControllerScript.cs
@@ -3,23 +3,58 @@ using System.Collections;
 
 public class characterControllerScript : MonoBehaviour {
 	
+	public float moveSpeed = 10f;
+	public float turnSpeed = 4f;
+	private Vector3 moveDirection;
+
+	void Start() {
+
+		// moveDirection = Vector3.right;
+
+	}
+	
+	void Update() {
+
+		Vector3 currentPosition = transform.position;
+
+		if (Input.GetButton ("Fire1")) {
+				
+			Vector3 moveToward = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			moveDirection = moveToward - currentPosition;
+			moveDirection.z = 0;
+			moveDirection.Normalize();
+
+		}
+
+		float targetAngle = Mathf.Atan2 (moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0, 0, targetAngle), turnSpeed * Time.deltaTime);
+
+		Vector3 target = moveDirection * moveSpeed + currentPosition;
+		transform.position = Vector3.Lerp (currentPosition, target, Time.deltaTime);
+	}
+
+	/*
 	//Public Vars
 	public Camera camera;
 	public float speed;
-	
+
 	//Private Vars
 	private Vector3 mousePosition;
 	private Vector3 direction;
 	private float distanceFromObject;
 	private float t;
+	private Transform myTransform;
+	private Vector3 destinationPosition;
 
 	void Start() {
 
 		t = Time.time;
+		myTransform = transform;
+		destinationPosition = transform.position;
 
 	}
 
-	void FixedUpdate() {
+	void Update() {
 		
 		if (Input.GetButton("Fire1")){
 			
@@ -36,13 +71,15 @@ public class characterControllerScript : MonoBehaviour {
 
 			//Move towards the mouse
 			// rigidbody2D.AddForce(direction * speed * distanceFromObject * Time.deltaTime);
+			// rigidbody2D.AddForce(new Vector2(10f, 10f));
 
+			destinationPosition = mousePosition;
 
 			// Debug.Log(Vector3.Lerp(transform.position, mousePosition, 10f));
 
 			// transform.Translate(Vector3.Lerp(transform.position, mousePosition, (t - Time.time)));
 
-			transform.position = Vector3.Lerp(transform.position, mousePosition, (t - Time.time));
+			myTransform.position = Vector2.MoveTowards(myTransform.position, destinationPosition, speed * Time.deltaTime);
 			// rigidbody2D.velocity = Vector2.Lerp(transform.position, mousePosition, speed);
 
 			// rigidbody2D.velocity = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
@@ -50,5 +87,7 @@ public class characterControllerScript : MonoBehaviour {
 		}//End Move Towards If Case
 		
 	}//End Fire3 If case
+
+	*/
 }
 
